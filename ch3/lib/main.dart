312e3a0c1d3ce270e32a9610b3b5270f85f24133
yc5718 <ldm7718@naver.com>
2023-10-10 17:44:18 +0900

    hello

--- ch3/lib/main.dart
+++ ch3/lib/main.dart
@@ -1,56 +1,46 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter Demo",
-      home: HelloPage()
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
     );
   }
 }
 
-class HelloPage extends StatefulWidget{
-  late String title;
-
-  HelloPage({super.key, String title}){
-    this.title = title;
-  }
-
-  State<HelloPage> createState() => _HelloPageState();
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _HelloPageState extends State<HelloPage> {
-  Widget build(BuildContext context) {
+class _MyHomePageState extends State<MyHomePage> {
+  var _text = 'Hello';
+
+  @override
+  Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
         title: Text('Hello World'),
       ),
       body: Text(
-        message,
-        style: TextStyle(fontSize: 58),
+        _text,
+        style: TextStyle(fontSize: 40),
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icon.add),
-        Onpressed: (){
+        onPressed: () {
           setState(() {
-          message = "헬로 월드";
-             }
-        }
+            _text = 'World';
+          });
+        },
+        child: Icon(Icons.touch_app),
       ),
     );
-  }
-
-  void ChangeMessage(){
-    setState(() {
-      message = "헬로 월드";
-    });
   }
 }
