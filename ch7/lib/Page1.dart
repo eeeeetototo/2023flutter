5cdbafa5a3aefb48c1fb92f7bd0c0032dd55c911
yc5718 <ldm7718@naver.com>
2023-11-21 17:45:32 +0900

    11/21

--- ch7/lib/Page1.dart
+++ ch7/lib/Page1.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class Page1 extends StatelessWidget {
   const Page1({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: [
         _buildTop(),
         _buildMiddle(),
@@ -113,11 +114,50 @@ class Page1 extends StatelessWidget {
   }
 
   Widget _buildMiddle(){
-    return Text('Middle');
+    final imageItems = [
+      'assets/korea1.jpg',
+      'assets/korea2.jpg',
+      'assets/korea3.jpg',
+    ];
+
+    return CarouselSlider( //사진 입력
+      options: CarouselOptions(
+        height: 500.0,
+      ),
+      items: imageItems.map((path)
+      {
+        return Builder(
+            builder: (BuildContext context) {
+              return Container(
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.symmetric(horizontal: 5.0),
+                child: ClipRect(
+                  child: Image.asset(
+                      path,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              );
+            },
+        );
+      }
+      ).toList(),
+    );
   }
 
   Widget _buildBottom(){
-    return Text('Bottom');
+    final items = List.generate(
+        10, (i){
+          return ListTile(
+            leading: Icon(Icons.notifications_none),
+            title: Text('공지사항입니다.'),
+          );
+    });
+    return ListView(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      children: items,
+    );
   }
 }
 
